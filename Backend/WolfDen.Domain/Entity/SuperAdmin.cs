a6d7c07e00fe8c7c7ad2a8134297a0b41ca4e2bf
abhishek-geekywolf <abhishek.sadasivan@geekywolf.com>
2024-11-12 16:17:25 +0530

    backend first

---
+++ Backend/WolfDen.Domain/Entity/SuperAdmin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WolfDen.Domain.Entity;
+
+namespace EmployeeManagementDomain.Entity
+{
+    public class SuperAdmin
+    {
+        public int Id { get;private set; }
+        public int EmployeeId { get; private set; }
+        public Employee Employee { get; private set; }
+        public SuperAdmin() { }
+        public SuperAdmin(int employeeId)
+        { 
+            EmployeeId=employeeId;
+
+        }
+    }
+}
