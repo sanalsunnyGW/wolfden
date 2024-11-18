678aa447fddb29be1fa23f263d7c0a339d4a92c8
mukhil geeky <mukhil.mp@geekywolf.com>
2024-11-18 17:41:51 +0530

    Removed migrations

---
+++ src/services/WolfDen.Application/Requests/Commands/LeaveManagement/LeaveRequests/AddLeaveRequest/AddLeaveRequestCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace WolfDen.Application.Requests.Commands.LeaveManagement.LeaveRequests.AddLeaveRequest
+{
+    public class AddLeaveRequestCommandHandler : IRequestHandler<AddLeaveRequestCommand, string>
+    {
+        public Task<string> Handle(AddLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
