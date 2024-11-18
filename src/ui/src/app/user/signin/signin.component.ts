d30511c3fcb9b05eeb7555199de42a6861bd1724
Nohan Antony <nohan.antony@geekywolf.com>
2024-11-18 18:02:28 +0530

    Corrections

--- src/ui/src/app/user/signin/signin.component.ts
+++ src/ui/src/app/user/signin/signin.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
-
+  userForm : FormGroup<SignUpForm>;
 }
