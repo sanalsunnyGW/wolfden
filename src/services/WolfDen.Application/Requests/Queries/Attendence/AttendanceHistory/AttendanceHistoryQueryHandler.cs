12df1b8e5daa5027884d7c9c85391fc0ce63af40
athullya.r <athullya.r@CQLAP-52>
2024-11-21 15:22:20 +0530

    initial pagination done

--- src/services/WolfDen.Application/Requests/Queries/Attendence/AttendanceHistory/AttendanceHistoryQueryHandler.cs
+++ src/services/WolfDen.Application/Requests/Queries/Attendence/AttendanceHistory/AttendanceHistoryQueryHandler.cs
@@ -10,11 +10,33 @@ namespace WolfDen.Application.Requests.Queries.Attendence.AttendanceHistory
         private readonly WolfDenContext _context=context;
         public async Task<List<AttendanceHistoryDTO>> Handle(AttendanceHistoryQuery request, CancellationToken cancellationToken)
         {
+            DateOnly yearStart = new DateOnly(request.Year,1,1);
+            DateOnly yearEnd = new DateOnly(request.Year+1,1,0);
+
             var dailyAttendance =await _context.DailyAttendence
-                .Where(x=>x.EmployeeId == request.EmployeeId)
+                .Where(x=>x.EmployeeId == request.EmployeeId && x.Date >= yearStart && x.Date <=yearEnd)
+                .Select(x=> new AttendanceHistoryDTO
+                {
+                    ArrivalTime = x.ArrivalTime,
+                    DepartureTime = x.DepartureTime,
+                    InsideHours = x.InsideDuration,
+                    OutsideHours = x.OutsideDuration,
+                    MissedPunch = x.MissedPunch,
+
+                })
                 .ToListAsync();
 
+            int totalCount=dailyAttendance.Count;
+            int perPageCount = 15;
 
+            var records = new List<AttendanceHistoryDTO>
+            {
+                Date
+            };
+            
+            var displayAttendance = dailyAttendance.Skip(request.CurrentPage * perPageCount).Take(perPageCount).ToList();
+
+            return records;
         }
     }
 }
