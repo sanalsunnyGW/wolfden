4b05835816e8a1496c3a82c9483bb7e31729e7af
athullya.r <athullya.r@CQLAP-52>
2024-11-22 13:14:30 +0530

    removed unused imports

--- src/services/WolfDen.Application/DTOs/Attendence/AttendanceHistoryDTO.cs
+++ src/services/WolfDen.Application/DTOs/Attendence/AttendanceHistoryDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WolfDen.Application.DTOs.Attendence
+﻿namespace WolfDen.Application.DTOs.Attendence
 {
     public class AttendanceHistoryDTO
     {
