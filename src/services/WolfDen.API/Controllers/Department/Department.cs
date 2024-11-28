98d77e23cc246706b70c1850382bff63bc70f415
Nohan Antony <nohan.antony@geekywolf.com>
2024-11-28 12:27:18 +0530

    corrections

--- src/services/WolfDen.API/Controllers/Department/Department.cs
+++ src/services/WolfDen.API/Controllers/Department/Department.cs
@@ -20,7 +20,7 @@ namespace WolfDen.API.Controllers.Department
             return await _mediator.Send(command,cancellationToken);
         }
 
-        [HttpGet("all")]
+        [HttpGet]
         public async Task<ActionResult<List<DepartmentDTO>>> GetAllDepartment([FromQuery] GetAllDepartmentQuery query, CancellationToken cancellationToken)
         {
             return await _mediator.Send(query, cancellationToken);
