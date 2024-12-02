1f1eaba945d907813c5ae81e59bc908749b4ac8e
athullya.r <athullya.r@CQLAP-52>
2024-12-02 11:38:28 +0530

    get holiday done

---
+++ src/services/WolfDen.Application/Requests/Queries/Attendence/GetHolidays/GetHolidayQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using WolfDen.Application.DTOs.Attendence;
+
+namespace WolfDen.Application.Requests.Queries.Attendence.GetHolidays
+{
+    public class GetHolidayQuery:IRequest<List<HolidayDTO>>
+    {
+        public int Year { get; set; }
+    }
+}
---
+++ src/services/WolfDen.Application/Requests/Queries/Attendence/GetHolidays/GetHolidayQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using WolfDen.Application.DTOs.Attendence;
+using WolfDen.Infrastructure.Data;
+
+namespace WolfDen.Application.Requests.Queries.Attendence.GetHolidays
+{
+    public class GetHolidayQueryHandler(WolfDenContext context) : IRequestHandler<GetHolidayQuery, List<HolidayDTO>>
+    {
+        private readonly WolfDenContext _context=context;
+        public async Task<List<HolidayDTO>> Handle(GetHolidayQuery request, CancellationToken cancellationToken)
+        {
+            DateOnly yearStart = new DateOnly(request.Year, 1, 1);
+            DateOnly yearEnd = new DateOnly(request.Year+1, 1, 1);
+
+            List<HolidayDTO> holidayList = new List<HolidayDTO>();
+
+            var holidays = await _context.Holiday
+                .Where(x=>x.Date >= yearStart && x.Date < yearEnd)
+                .ToListAsync(cancellationToken);
+
+            DateOnly today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            for (var currentDate = yearStart; currentDate < yearEnd; currentDate.AddDays(1))
+            {
+                if (currentDate < today)
+                    continue;
+
+                holidayList = holidays
+                    .Where(x => x.Date >= today && x.Date < yearEnd)
+                    .Select(x => new HolidayDTO
+                    {
+                        Date = x.Date,
+                        Type = x.Type
+                    }).ToList();
+            }
+
+            return holidayList;
+        }
+    }
+}
